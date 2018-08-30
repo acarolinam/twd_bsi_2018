5fffd282562f23a1c3a6e850c457b6f41bd246a8
Bruno <bruno.tissi@hotmail.com>
2018-08-30 11:46:46 -0300

    Trabalhando com templates, static e media

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {}
+    context_dict['boldmessage'] = "Ola BSI6, este texto veio da view"
+    context_dict["autor"] = "Bruno Tissi"
+
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    texto = '''
+    <h1> Sobre o Rango </h1>
+    Essa e a pagina sobre o Rango.
+    <br/><a href='/rango/'>Index</a>
+    '''
+    return HttpResponse(texto)
