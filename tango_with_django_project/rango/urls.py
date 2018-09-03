1663fa92f8c8883d1e28bb8257f7586045969df1
Ana Carolina Mateus <acarolinamat@gmail.com>
2018-09-03 11:05:08 -0300

    Templates e media

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -3,4 +3,5 @@ from rango import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url('about', views.about, name='about'),
 ]
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,17 +1,12 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-def index(request):
-    context_dict = {}
-    context_dict['boldmessage'] = "Ola BSI6, este texto veio da view"
-    context_dict["autor"] = "Bruno Tissi"
 
-    return render(request, 'rango/index.html', context=context_dict)
+def index(request):
+     context_dict = {'boldmessage': "Olá BSI6, este texto veio da view!"}
+     return render(request, 'rango/index.html', context=context_dict)
+
 
 def about(request):
-    texto = '''
-    <h1> Sobre o Rango </h1>
-    Essa e a pagina sobre o Rango.
-    <br/><a href='/rango/'>Index</a>
-    '''
-    return HttpResponse(texto)
+     context_dict = {'autor': 'Ana Carolina e Bruno Tissi'}
+
+     return render(request, 'rango/about.html', context=context_dict)
