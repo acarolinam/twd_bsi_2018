84bdcb1a6edabca3d688445cf123a86503eff9d6
Ana Carolina Mateus <acarolinamat@gmail.com>
2018-09-03 11:39:15 -0300

    Primeira Model

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+            return self.name
+
+
+class Page(models.Model):
+    categoria = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def _str_(self):
+        return self.title
--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url('about', views.about, name='about'),
 ]
+
+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
