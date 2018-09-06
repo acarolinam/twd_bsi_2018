a1a32fde63f394bb9a589c035fbedd989f65c45c
Ana Carolina Mateus <acarolinamat@gmail.com>
2018-09-06 10:50:50 -0300

    Edição model e edição  do admin

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -5,6 +5,9 @@ class Category(models.Model):
 
     name = models.CharField(max_length=128, unique=True)
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     def __str__(self):
             return self.name
 
