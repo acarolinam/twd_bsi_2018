2249cfd33e2f7f057c0d6b759d58ae6de8718f96
Ana Carolina Mateus <acarolinamat@gmail.com>
2018-09-20 10:23:02 -0300

    Restrutura dos espaços de acordo com a Pep

--- tango_with_django_project/rango/views/about.py
+++ tango_with_django_project/rango/views/about.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 
 def about(request):
-    context_dict = {'autor': 'Marco André Mendes'}
+    context_dict = {'autor': 'Ana e Bruno'}
     return render(request, 'rango/about.html', context=context_dict)
